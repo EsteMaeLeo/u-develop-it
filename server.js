c739c2e857ce2c5bdd2cadc0bdf34b7dcff21c77
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-02-28 14:47:12 -0600

    server add connection and query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,10 +9,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL password
+        user: 'root',
+        //Your MySQL password
+        password: 'Sql2022',
+        database: 'election'
+    },
+    console.log('connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
